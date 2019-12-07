c8324200f2a85b084c6425774215253f3d792ee7
Richard <richard@hazelanalytics.com>
2019-12-06 23:00:38 -0800

    Complete faust_stream.py

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -30,28 +30,50 @@ class TransformedStation(faust.Record):
 
 
 # TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
-app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+#   places it into a new topic with only the necessary information. (DONE)
+app = faust.App("stations-stream", broker="kafka://172.31.1.20:9092", store="memory://")
+
+# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to? (DONE)
+topic = app.topic("org.chicago.cta.db.stations", value_type=Station)
+
+# TODO: Define the output Kafka Topic (DONE)
+out_topic = app.topic("org.chicago.cta.stations.table.v1", partitions=1)
+
+# TODO: Define a Faust Table (DONE)
+table = app.Table(
+    "transformed_statiosn",
+    default=TransformedStation,
+    partitions=1,
+    changelog_topic=out_topic,
+)
 
 
 #
 #
 # TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
 # "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
+# then you would set the `line` of the `TransformedStation` record to the string `"red"` (DONE)
 #
 #
+@app.agent(topic)
+async def process_stations(stations):
+    async for station in stations:
+        out_topic[station.station_id] = TransformedStation(
+            station_id=station.station_id,
+            station_name=station.station_name,
+            order=station.order,
+            line=get_line(station),
+        )
+
+
+def get_line(station):
+    if station.red:
+        return "red"
+    if station.blue:
+        return "blue"
+    if station.green:
+        return "green"
+    return None  # There are some records where red, blue, and green are all False
 
 
 if __name__ == "__main__":
